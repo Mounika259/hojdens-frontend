ad3b0a5b06c589a18aea5cf58cb99ae0d0a29b64
Mounika <mounika259@outlook.com>
2023-02-02 15:11:07 +0100

    showmore functionality

--- src/components/cardGrid.js
+++ src/components/cardGrid.js
@@ -1,43 +1,59 @@
-/* eslint-disable no-undef */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable no-console */
-import React, { useState } from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 import { Products } from '../products';
-import ShowMore from './showMore';
 
 function CardGrid(data) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8000/products').then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  }, []);
   // const [category, setCategory] = useState('');
+  const [index, setIndex] = useState(6);
+  const [isCompleted, setIsCompleted] = useState(false);
   console.log(data);
   const getColumnsForRow = () => {
     let items = [];
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(1);
     // eslint-disable-next-line react/destructuring-assignment
     if (data.data === 'noCategory') {
-      items = Products.slice(0, 5).map((product) => (
+      items = products.map((product) => (
         <div key={product.id}>
           <Card className="cardGrid text-center">
             <Card.Body>
-              <img className="productImg" src={product.images} alt="item" />
+              <p>
+                <img
+                  className="productImg"
+                  /* src={require(`./assets/images/${product.images}`).default} */
+                  src={product.images}
+                  alt="item"
+                />
+              </p>
               <Card.Title className="offer-title">{product.title}</Card.Title>
-              <Card.Text>{product.price}</Card.Text>
+              <Card.Text className="offer-price">{product.price}</Card.Text>
             </Card.Body>
           </Card>
         </div>
       ));
     }
     // eslint-disable-next-line react/destructuring-assignment
-    if (data.data === 'vegetables') {
+    if (data.data === 'Vegetables') {
       /* setCategory('vegetables');
       console.log(category); */
       const vegetables = Products.filter(
-        (product) => product.category === 'vegetables'
+        (product) => product.category === 'Vegetables'
       );
       items = vegetables.map((vegetable) => (
         <div key={vegetable.id}>
           <Card className="cardGrid text-center">
             <Card.Body>
               <img
+                className="productImg"
                 src={vegetable.images}
                 alt="item"
                 width="200px"
@@ -51,10 +67,10 @@ function CardGrid(data) {
       ));
     }
     // eslint-disable-next-line react/destructuring-assignment
-    if (data.data === 'snacks') {
+    if (data.data === 'Snacks') {
       // setCategory('cookies');
       const snacks = Products.filter(
-        (product) => product.category === 'snacks'
+        (product) => product.category === 'Snacks'
       );
       items = snacks.map((snack) => (
         <div key={snack.id}>
@@ -70,9 +86,9 @@ function CardGrid(data) {
       console.log(items);
     }
     // eslint-disable-next-line react/destructuring-assignment
-    if (data.data === 'meat') {
+    if (data.data === 'Meat') {
       // setCategory('meat');
-      const meat = Products.filter((product) => product.category === 'meat');
+      const meat = Products.filter((product) => product.category === 'Meat');
       items = meat.map((meatItems) => (
         <div key={meatItems.id}>
           <Card className="cardGrid text-center">
@@ -85,26 +101,53 @@ function CardGrid(data) {
         </div>
       ));
     }
-    if (items.length > 5) {
-      setCount(count + 1);
-    }
     return items;
   };
+
+  const loadMore = () => {
+    setIndex(index + 5);
+    console.log(index);
+    if (index >= Products.length) {
+      setIsCompleted(true);
+    } else {
+      setIsCompleted(false);
+    }
+  };
+  useEffect(() => {
+    getColumnsForRow();
+  }, []);
+
   return (
     <div>
       {data.data !== 'noCategory' ? <h3>{data.data}</h3> : null}
-
       <div className="row">
         <Row>
           <Col className="cardGrid">{getColumnsForRow()}</Col>
-          {/*  <Col>
-            {count >= 1 ? (
-              <div>
-                <ShowMore />
-              </div>
-            ) : null}
-          </Col> */}
-          <ShowMore />
+        </Row>
+        <Row>
+          <Col>
+            <div className="text-center mt-4 mb-4">
+              {isCompleted ? (
+                <Button
+                  className="btn-danger offer-btn disabled"
+                  size="sm"
+                  onClick={loadMore}
+                  type="button"
+                >
+                  Show More
+                </Button>
+              ) : (
+                <Button
+                  className="btn-danger offer-btn"
+                  size="sm"
+                  onClick={loadMore}
+                  type="button"
+                >
+                  Show More
+                </Button>
+              )}
+            </div>
+          </Col>
         </Row>
       </div>
     </div>
